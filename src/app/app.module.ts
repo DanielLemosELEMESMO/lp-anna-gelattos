58862d3b45237f319773e6ca35af41f642018265
DanielLemosELEMESMO <daniel.dcl.es@gmail.com>
2021-11-22 17:24:48 -0300

    trabalho para home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FullBannerComponent } from './full-banner/full-banner.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ProdutoComponent } from './produto/produto.component';
 import { PontoForteComponent } from './ponto-forte/ponto-forte.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormularioContatoComponent } from './formulario-contato/formulario-contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
     LandingPageComponent,
     FullBannerComponent,
     NavBarComponent,
+    PontoForteComponent,
     ProdutoComponent,
-    PontoForteComponent
+    PageNotFoundComponent,
+    FormularioContatoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulario-contato/formulario-contato.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-contato',
+  templateUrl: './formulario-contato.component.html',
+  styleUrls: ['./formulario-contato.component.scss']
+})
+export class FormularioContatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
